1b7e623e656fc1562e3e083119984a6f99c3c1c2
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-06-29 16:42:28 +0200

    [pulpino] Vivado 2015.2

--- vsim/tcl_files/sig.tcl
+++ vsim/tcl_files/sig.tcl
@@ -40,16 +40,4 @@ sim:/tb/top_i/peripherals_i/apb_monitor_i/E4 \
 sim:/tb/top_i/peripherals_i/apb_monitor_i/E5 \
 sim:/tb/top_i/peripherals_i/apb_monitor_i/E6 \
 sim:/tb/top_i/peripherals_i/apb_monitor_i/E7 \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_srunning \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_selected \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_running \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_elected \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_runningID \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_tpl_kern_electedID \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_tpl_kern_need_switch \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_tpl_kern_need_schedule \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_OS_instru_service \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_OS_instru_kernel_functions \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_return \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_config \
-sim:/tb/top_i/peripherals_i/apb_monitor_i/ADDR_reg_return_2
+add wave /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop01 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop02 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop03 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop04 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop05 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop06 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop07 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop08 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop09 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop10 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop11 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop12 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop13 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop14 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop15 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop16 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop17 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop18 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop19 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop20 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop21 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop22 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop23 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop24 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop25 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop26 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop27 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop28 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop29 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop30 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop31 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop32 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop33 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop34 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop35 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop36 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop37 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop38 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop39 /tb/top_i/peripherals_i/apb_monitor_i/assert__Prop40
