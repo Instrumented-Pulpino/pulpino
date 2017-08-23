ce6f4d3d9126e2c7c33831d9dc543e8f635dad87
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-08-23 14:45:31 +0200

    [RAM] Exporting DDR AXI interface in PULPino

--- fpga/rtl/pulpino_wrap.v
+++ fpga/rtl/pulpino_wrap.v
@@ -61,6 +61,55 @@ module pulpino(
 
   monitor_valid,
 
+  AXI_DDR_awaddr,
+  AXI_DDR_awprot,
+  AXI_DDR_awregion,
+  AXI_DDR_awlen,
+  AXI_DDR_awsize,
+  AXI_DDR_awburst,
+  AXI_DDR_awlock,
+  AXI_DDR_awcache,
+  AXI_DDR_awqos,
+  AXI_DDR_awid,
+  AXI_DDR_awuser,
+  AXI_DDR_awready,
+  AXI_DDR_awvalid,
+
+  AXI_DDR_araddr,
+  AXI_DDR_arprot,
+  AXI_DDR_arregion,
+  AXI_DDR_arlen,
+  AXI_DDR_arsize,
+  AXI_DDR_arburst,
+  AXI_DDR_arlock,
+  AXI_DDR_arcache,
+  AXI_DDR_arqos,
+  AXI_DDR_arid,
+  AXI_DDR_aruser,
+  AXI_DDR_arready,
+  AXI_DDR_arvalid,
+
+  AXI_DDR_wvalid,
+  AXI_DDR_wdata,
+  AXI_DDR_wstrb,
+  AXI_DDR_wuser,
+  AXI_DDR_wlast,
+  AXI_DDR_wready,
+
+  AXI_DDR_rdata,
+  AXI_DDR_rresp,
+  AXI_DDR_rlast,
+  AXI_DDR_rid,
+  AXI_DDR_ruser,
+  AXI_DDR_rready,
+  AXI_DDR_rvalid,
+
+  AXI_DDR_bresp,
+  AXI_DDR_bid,
+  AXI_DDR_buser,
+  AXI_DDR_bready,
+  AXI_DDR_bvalid,
+
   tck_i,
   trstn_i,
   tms_i,
@@ -120,6 +169,55 @@ module pulpino(
   output [31:0] gpio_dir;
 
   output        monitor_valid;
+
+  output [31:0] AXI_DDR_awaddr;
+  output [2:0]  AXI_DDR_awprot;
+  output [3:0]  AXI_DDR_awregion;
+  output [7:0]  AXI_DDR_awlen;
+  output [2:0]  AXI_DDR_awsize;
+  output [1:0]  AXI_DDR_awburst;
+  output        AXI_DDR_awlock;
+  output [3:0]  AXI_DDR_awcache;
+  output [3:0]  AXI_DDR_awqos;
+  output [3:0]  AXI_DDR_awid;
+  output [0:0]  AXI_DDR_awuser;
+  input         AXI_DDR_awready;
+  output        AXI_DDR_awvalid;
+
+  output [31:0] AXI_DDR_araddr;
+  output [2:0]  AXI_DDR_arprot;
+  output [3:0]  AXI_DDR_arregion;
+  output [7:0]  AXI_DDR_arlen;
+  output [2:0]  AXI_DDR_arsize;
+  output [1:0]  AXI_DDR_arburst;
+  output        AXI_DDR_arlock;
+  output [3:0]  AXI_DDR_arcache;
+  output [3:0]  AXI_DDR_arqos;
+  output [3:0]  AXI_DDR_arid;
+  output [0:0]  AXI_DDR_aruser;
+  input         AXI_DDR_arready;
+  output        AXI_DDR_arvalid;
+
+  output        AXI_DDR_wvalid;
+  output [31:0] AXI_DDR_wdata;
+  output [3:0]  AXI_DDR_wstrb;
+  output [3:0]  AXI_DDR_wuser;
+  output        AXI_DDR_wlast;
+  input         AXI_DDR_wready;
+
+  input [31:0]  AXI_DDR_rdata;
+  input [1:0]   AXI_DDR_rresp;
+  input         AXI_DDR_rlast;
+  input [3:0]   AXI_DDR_rid;
+  input [0:0]   AXI_DDR_ruser;
+  output        AXI_DDR_rready;
+  input         AXI_DDR_rvalid;
+
+  input [1:0]   AXI_DDR_bresp;
+  input [3:0]   AXI_DDR_bid;
+  input [0:0]   AXI_DDR_buser;
+  output        AXI_DDR_bready;
+  input         AXI_DDR_bvalid;
 
   // JTAG signals
   input  tck_i;
@@ -188,6 +286,55 @@ module pulpino(
     .gpio_padcfg       (                   ),
 
     .monitor_valid     ( monitor_valid     ),
+
+  .AXI_DDR_awaddr ( AXI_DDR_awaddr ),
+  .AXI_DDR_awprot ( AXI_DDR_awprot ),
+  .AXI_DDR_awregion ( AXI_DDR_awregion ),
+  .AXI_DDR_awlen ( AXI_DDR_awlen ),
+  .AXI_DDR_awsize ( AXI_DDR_awsize ),
+  .AXI_DDR_awburst ( AXI_DDR_awburst ),
+  .AXI_DDR_awlock ( AXI_DDR_awlock ),
+  .AXI_DDR_awcache ( AXI_DDR_awcache ),
+  .AXI_DDR_awqos ( AXI_DDR_awqos ),
+  .AXI_DDR_awid ( AXI_DDR_awid ),
+  .AXI_DDR_awuser ( AXI_DDR_awuser ),
+  .AXI_DDR_awready ( AXI_DDR_awready ),
+  .AXI_DDR_awvalid ( AXI_DDR_awvalid ),
+
+  .AXI_DDR_araddr ( AXI_DDR_araddr ),
+  .AXI_DDR_arprot ( AXI_DDR_arprot ),
+  .AXI_DDR_arregion ( AXI_DDR_arregion ),
+  .AXI_DDR_arlen ( AXI_DDR_arlen ),
+  .AXI_DDR_arsize ( AXI_DDR_arsize ),
+  .AXI_DDR_arburst ( AXI_DDR_arburst ),
+  .AXI_DDR_arlock ( AXI_DDR_arlock ),
+  .AXI_DDR_arcache ( AXI_DDR_arcache ),
+  .AXI_DDR_arqos ( AXI_DDR_arqos ),
+  .AXI_DDR_arid ( AXI_DDR_arid ),
+  .AXI_DDR_aruser ( AXI_DDR_aruser ),
+  .AXI_DDR_arready ( AXI_DDR_arready ),
+  .AXI_DDR_arvalid ( AXI_DDR_arvalid ),
+
+  .AXI_DDR_wvalid ( AXI_DDR_wvalid ),
+  .AXI_DDR_wdata ( AXI_DDR_wdata ),
+  .AXI_DDR_wstrb ( AXI_DDR_wstrb ),
+  .AXI_DDR_wuser ( AXI_DDR_wuser ),
+  .AXI_DDR_wlast ( AXI_DDR_wlast ),
+  .AXI_DDR_wready ( AXI_DDR_wready ),
+
+  .AXI_DDR_rdata ( AXI_DDR_rdata ),
+  .AXI_DDR_rresp ( AXI_DDR_rresp ),
+  .AXI_DDR_rlast ( AXI_DDR_rlast ),
+  .AXI_DDR_rid ( AXI_DDR_rid ),
+  .AXI_DDR_ruser ( AXI_DDR_ruser ),
+  .AXI_DDR_rready ( AXI_DDR_rready ),
+  .AXI_DDR_rvalid ( AXI_DDR_rvalid ),
+
+  .AXI_DDR_bresp ( AXI_DDR_bresp ),
+  .AXI_DDR_bid ( AXI_DDR_bid ),
+  .AXI_DDR_buser ( AXI_DDR_buser ),
+  .AXI_DDR_bready ( AXI_DDR_bready ),
+  .AXI_DDR_bvalid ( AXI_DDR_bvalid ),
 
     .tck_i             ( tck_i             ),
     .trstn_i           ( trstn_i           ),
--- rtl/top.sv
+++ rtl/top.sv
@@ -23,24 +23,24 @@ module pulpino_top
     input logic               clk,
     input logic               rst_n,
 
-    input  logic              clk_sel_i,
-    input  logic              clk_standalone_i,
-    input  logic              testmode_i,
-    input  logic              fetch_enable_i,
-    input  logic              scan_enable_i,
+    input logic               clk_sel_i,
+    input logic               clk_standalone_i,
+    input logic               testmode_i,
+    input logic               fetch_enable_i,
+    input logic               scan_enable_i,
 
     //SPI Slave
-    input  logic              spi_clk_i,
-    input  logic              spi_cs_i,
+    input logic               spi_clk_i,
+    input logic               spi_cs_i,
     output logic [1:0]        spi_mode_o,
     output logic              spi_sdo0_o,
     output logic              spi_sdo1_o,
     output logic              spi_sdo2_o,
     output logic              spi_sdo3_o,
-    input  logic              spi_sdi0_i,
-    input  logic              spi_sdi1_i,
-    input  logic              spi_sdi2_i,
-    input  logic              spi_sdi3_i,
+    input logic               spi_sdi0_i,
+    input logic               spi_sdi1_i,
+    input logic               spi_sdi2_i,
+    input logic               spi_sdi3_i,
 
     //SPI Master
     output logic              spi_master_clk_o,
@@ -53,43 +53,92 @@ module pulpino_top
     output logic              spi_master_sdo1_o,
     output logic              spi_master_sdo2_o,
     output logic              spi_master_sdo3_o,
-    input  logic              spi_master_sdi0_i,
-    input  logic              spi_master_sdi1_i,
-    input  logic              spi_master_sdi2_i,
-    input  logic              spi_master_sdi3_i,
+    input logic               spi_master_sdi0_i,
+    input logic               spi_master_sdi1_i,
+    input logic               spi_master_sdi2_i,
+    input logic               spi_master_sdi3_i,
 
-    input  logic              scl_pad_i,
+    input logic               scl_pad_i,
     output logic              scl_pad_o,
     output logic              scl_padoen_o,
-    input  logic              sda_pad_i,
+    input logic               sda_pad_i,
     output logic              sda_pad_o,
     output logic              sda_padoen_o,
 
     output logic              uart_tx,
-    input  logic              uart_rx,
+    input logic               uart_rx,
     output logic              uart_rts,
     output logic              uart_dtr,
-    input  logic              uart_cts,
-    input  logic              uart_dsr,
+    input logic               uart_cts,
+    input logic               uart_dsr,
 
-    input  logic       [31:0] gpio_in,
-    output logic       [31:0] gpio_out,
-    output logic       [31:0] gpio_dir,
+    output logic [31:0]       AXI_DDR_awaddr,
+    output logic [2:0]        AXI_DDR_awprot,
+    output logic [3:0]        AXI_DDR_awregion,
+    output logic [7:0]        AXI_DDR_awlen,
+    output logic [2:0]        AXI_DDR_awsize,
+    output logic [1:0]        AXI_DDR_awburst,
+    output logic              AXI_DDR_awlock,
+    output logic [3:0]        AXI_DDR_awcache,
+    output logic [3:0]        AXI_DDR_awqos,
+    output logic [3:0]        AXI_DDR_awid,
+    output logic [0:0]        AXI_DDR_awuser,
+    input logic               AXI_DDR_awready,
+    output logic              AXI_DDR_awvalid,
+
+    output logic [31:0]       AXI_DDR_araddr,
+    output logic [2:0]        AXI_DDR_arprot,
+    output logic [3:0]        AXI_DDR_arregion,
+    output logic [7:0]        AXI_DDR_arlen,
+    output logic [2:0]        AXI_DDR_arsize,
+    output logic [1:0]        AXI_DDR_arburst,
+    output logic              AXI_DDR_arlock,
+    output logic [3:0]        AXI_DDR_arcache,
+    output logic [3:0]        AXI_DDR_arqos,
+    output logic [3:0]        AXI_DDR_arid,
+    output logic [0:0]        AXI_DDR_aruser,
+    input logic               AXI_DDR_arready,
+    output logic              AXI_DDR_arvalid,
+
+    output logic              AXI_DDR_wvalid,
+    output logic [31:0]       AXI_DDR_wdata,
+    output logic [3:0]        AXI_DDR_wstrb,
+    output logic [3:0]        AXI_DDR_wuser,
+    output logic              AXI_DDR_wlast,
+    input logic               AXI_DDR_wready,
+
+    input logic [31:0]        AXI_DDR_rdata,
+    input logic [1:0]         AXI_DDR_rresp,
+    input logic               AXI_DDR_rlast,
+    input logic [3:0]         AXI_DDR_rid,
+    input logic [0:0]         AXI_DDR_ruser,
+    output logic              AXI_DDR_rready,
+    input logic               AXI_DDR_rvalid,
+
+    input logic [1:0]         AXI_DDR_bresp,
+    input logic [3:0]         AXI_DDR_bid,
+    input logic [0:0]         AXI_DDR_buser,
+    output logic              AXI_DDR_bready,
+    input logic               AXI_DDR_bvalid,
+
+    input logic [31:0]        gpio_in,
+    output logic [31:0]       gpio_out,
+    output logic [31:0]       gpio_dir,
     output logic [31:0] [5:0] gpio_padcfg,
 
-    // JTAG signals
-    input  logic              tck_i,
-    input  logic              trstn_i,
-    input  logic              tms_i,
-    input  logic              tdi_i,
+   // JTAG signals
+    input logic               tck_i,
+    input logic               trstn_i,
+    input logic               tms_i,
+    input logic               tdi_i,
     output logic              tdo_o,
 
-    // Monitor signal
-    output logic               monitor_valid,
+   // Monitor signal
+    output logic              monitor_valid,
 
-    // PULPino specific pad config
+   // PULPino specific pad config
     output logic [31:0] [5:0] pad_cfg_o,
-    output logic       [31:0] pad_mux_o
+    output logic [31:0]       pad_mux_o
   );
 
   logic        clk_int;
@@ -193,6 +242,51 @@ module pulpino_top
     .tdi_i          ( tdi_i             ),
     .tdo_o          ( tdo_o             )
   );
+
+  assign AXI_DDR_awaddr = slaves[1].aw_addr;
+  assign AXI_DDR_awprot = slaves[1].aw_prot;
+  assign AXI_DDR_awregion = slaves[1].aw_region;
+  assign AXI_DDR_awlen = slaves[1].aw_len;
+  assign AXI_DDR_awsize = slaves[1].aw_size;
+  assign AXI_DDR_awburst = slaves[1].aw_burst;
+  assign AXI_DDR_awlock = slaves[1].aw_lock;
+  assign AXI_DDR_awcache = slaves[1].aw_cache;
+  assign AXI_DDR_awqos = slaves[1].aw_qos;
+  assign AXI_DDR_awid = slaves[1].aw_id;
+  assign AXI_DDR_awuser = slaves[1].aw_user;
+  assign slaves[1].aw_ready = AXI_DDR_awready;
+  assign AXI_DDR_awvalid = slaves[1].aw_valid;
+  assign AXI_DDR_araddr = slaves[1].ar_addr;
+  assign AXI_DDR_arprot = slaves[1].ar_prot;
+  assign AXI_DDR_arregion = slaves[1].ar_region;
+  assign AXI_DDR_arlen = slaves[1].ar_len;
+  assign AXI_DDR_arsize = slaves[1].ar_size;
+  assign AXI_DDR_arburst = slaves[1].ar_burst;
+  assign AXI_DDR_arlock = slaves[1].ar_lock;
+  assign AXI_DDR_arcache = slaves[1].ar_cache;
+  assign AXI_DDR_arqos = slaves[1].ar_qos;
+  assign AXI_DDR_arid = slaves[1].ar_id;
+  assign AXI_DDR_aruser = slaves[1].ar_user;
+  assign slaves[1].ar_ready = AXI_DDR_arready;
+  assign AXI_DDR_arvalid = slaves[1].ar_valid;
+  assign AXI_DDR_wvalid = slaves[1].w_valid;
+  assign AXI_DDR_wdata = slaves[1].w_data;
+  assign AXI_DDR_wstrb = slaves[1].w_strb;
+  assign AXI_DDR_wuser = slaves[1].w_user;
+  assign AXI_DDR_wlast = slaves[1].w_last;
+  assign slaves[1].w_ready = AXI_DDR_wready;
+  assign slaves[1].r_data = AXI_DDR_rdata;
+  assign slaves[1].r_resp = AXI_DDR_rresp;
+  assign slaves[1].r_last = AXI_DDR_rlast;
+  assign slaves[1].r_id = AXI_DDR_rid;
+  assign slaves[1].r_user = AXI_DDR_ruser;
+  assign AXI_DDR_rready = slaves[1].r_ready;
+  assign slaves[1].r_valid = AXI_DDR_rvalid;
+  assign slaves[1].b_resp = AXI_DDR_bresp;
+  assign slaves[1].b_id = AXI_DDR_bid;
+  assign slaves[1].b_user = AXI_DDR_buser;
+  assign AXI_DDR_bready = slaves[1].b_ready;
+  assign slaves[1].b_valid = AXI_DDR_bvalid;
 
   //----------------------------------------------------------------------------//
   // Peripherals
@@ -302,11 +396,11 @@ module pulpino_top
     .rst_n     ( rstn_int   ),
     .test_en_i ( testmode_i ),
 
-    .master    ( slaves     ),
-    .slave     ( masters    ),
+    .master    (   slaves     ),
+    .slave     (   masters    ),
 
     .start_addr_i ( { 32'h1000_0000, 32'h1A10_0000 } ),
-    .end_addr_i   ( { 32'h1FFF_FFFF, 32'h1A11_FFFF } )
+    .end_addr_i   ( { 32'h17FF_FFFF, 32'h1A11_FFFF } )
   );
 
 endmodule
